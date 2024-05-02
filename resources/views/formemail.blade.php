113bca5637a83442f6b34d973f8c9e0c758799e4
dwiky <dwiky@jayantara.net>
2024-05-02 15:49:41 +0700

    Penambahan script tailwind dan bs

--- resources/views/formemail.blade.php
+++ resources/views/formemail.blade.php
@@ -3,39 +3,36 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Tutorial Laravel Mail | ayongoding.com</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <title>Blaster Email</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <style>
+    .middle{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width:100vw;
+        
+    }
+  </style>
 </head>
 <body>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-6">
-	<h3>Tinggalkan Balasan</h3>
-	<form method="post" action="/kirim">
-	  @csrf
-	  <div class="form-group">
-	    <label>Nama Lengkap</label>
-	    <input class="form-control" type="text" name="nama">
-	  </div>
-	  <div class="form-group">
-	    <label>Website</label>
-	    <input class="form-control" type="text" name="website">
-	  </div>
-	  <div class="form-group">
-            <label>Komentar</label>
-	    <textarea class="form-control" name="komentar" rows="4"></textarea>
-	  </div>
-	  <div class="form-group">
-	    <label>Alamat Email Saya</label>
-	    <input class="form-control" type="text" name="email">
-	  </div>
-	  <div class="text-right">
-	    <button type="submit" class="btn btn-primary">Kirim ke Email Saya</button>
-	  </div>
-	</form>
-      </div>
-    </div>
+  <div class="middle">
+    <div class="card">
+        <div class=" font-bold underline">
+            Blaster Email
+        </div>
+        <form method="post" action="/kirim">
+            @csrf
+                <div class="form-group">
+                    <label>Alamat Email Saya</label>
+                    <input class="form-control" type="text" name="email" required>
+                </div>
+                <div class="text-right">
+                    <button type="submit" class="btn btn-primary">Kirim ke Email Saya</button>
+                </div>
+        </form>
   </div>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
